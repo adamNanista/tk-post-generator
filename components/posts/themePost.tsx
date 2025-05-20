a8800309941f4eaff3960a2478ca0a5759ae72b3
Adam Naništa <nanista@totalmoney.sk>
2025-05-20 17:31:38 +0200

    Controls

--- components/posts/themePost.tsx
+++ components/posts/themePost.tsx
@@ -6,12 +6,22 @@ import { useUIStore } from "@/stores/useUIStore";
 
 import { parseProgramItems } from "@/lib/parseProgram";
 
+import ColorPicker from "@/components/controls/colorPicker";
+import WidthControl from "@/components/controls/widthControl";
+import SpaceControl from "@/components/controls/spaceControl";
+import ColorControl from "@/components/controls/colorControl";
+import SizeControl from "@/components/controls/sizeControl";
+
 import CompanyLogo from "@/components/elements/companyLogo";
 import EventLogo from "@/components/elements/eventLogo";
 import Theme from "@/components/elements/theme";
 
 import Button from "@mui/material/Button";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 import DownloadIcon from "@mui/icons-material/Download";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function ThemePost({ data }: { data: any }) {
 	const parsedItems = parseProgramItems(data.event.program);
@@ -40,6 +50,35 @@ export default function ThemePost({ data }: { data: any }) {
 				<div className="space-y-2">
 					<h1 className="text-2xl font-black text-pretty">{data.event.name}</h1>
 					<p>Posty s témami 1080x1350</p>
+				</div>
+				<div>
+					<Accordion disableGutters>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>Všeobecné</AccordionSummary>
+						<AccordionDetails>
+							<div className="space-y-6">
+								<ColorPicker label="Primary farba" name="primaryColor" valueGetter={(state) => state.primaryColor} valueSetter={(state) => state.setPrimaryColor} />
+								<ColorPicker label="Accent farba" name="accentColor" valueGetter={(state) => state.accentColor} valueSetter={(state) => state.setAccentColor} />
+							</div>
+						</AccordionDetails>
+					</Accordion>
+					<Accordion disableGutters>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>Logo TREND konferencie</AccordionSummary>
+						<AccordionDetails>
+							<div className="space-y-6">
+								<ColorControl label="Farba" name="companyLogoFill" valueGetter={(state) => state.companyLogoFill} valueSetter={(state) => state.setCompanyLogoFill} />
+								<SpaceControl label="Odsadenie" name="companyLogoSpace" spacesGetter={(state) => state.spaces} indexGetter={(state) => state.companyLogoSpaceIndex} indexSetter={(state) => state.setCompanyLogoSpaceIndex} />
+							</div>
+						</AccordionDetails>
+					</Accordion>
+					<Accordion disableGutters>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>Logo eventu</AccordionSummary>
+						<AccordionDetails>
+							<div className="space-y-6">
+								<WidthControl label="Šírka" name="eventLogoWidth" valueGetter={(state) => state.eventLogoWidth} valueSetter={(state) => state.setEventLogoWidth} />
+								<SpaceControl label="Odsadenie" name="eventLogoSpace" spacesGetter={(state) => state.spaces} indexGetter={(state) => state.eventLogoSpaceIndex} indexSetter={(state) => state.setEventLogoSpaceIndex} />
+							</div>
+						</AccordionDetails>
+					</Accordion>
 				</div>
 			</div>
 			<div className="flex flex-col items-center grow max-h-screen overflow-auto p-12 space-y-12">
